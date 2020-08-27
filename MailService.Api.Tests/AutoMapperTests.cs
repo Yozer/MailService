ce485a9d530ac1584f84ad94ea8519e42dd1b3ce
Dominik Baran <dominik.baran7@gmail.com>
2020-08-28 00:09:05 +0200

    tests: add automapper assertions

---
+++ MailService.Api.Tests/AutoMapperTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using MailService.Api.Infrastructure;
+using NUnit.Framework;
+
+namespace MailService.Api.Tests
+{
+    public class AutoMapperTests
+    {
+        [Test]
+        public void ValidateAutoMapper()
+        {
+            var profile = new AutoMapperProfile();
+            var configuration = new MapperConfiguration(t => t.AddProfile(profile));
+            configuration.AssertConfigurationIsValid();
+        }
+    }
+}
