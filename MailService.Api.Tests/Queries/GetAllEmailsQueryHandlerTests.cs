5b0453eda6a5c24010ccaf67640349a2d5143335
Dominik Baran <dominik.baran7@gmail.com>
2020-08-28 01:41:50 +0200

    tests: added tests for query handlers

---
+++ MailService.Api.Tests/Queries/GetAllEmailsQueryHandlerTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using MailService.Api.Model;
+using MailService.Api.Queries;
+using MailService.Api.Queries.Handlers;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace MailService.Api.Tests.Queries
+{
+    public class GetAllEmailsQueryHandlerTests : TestBase
+    {
+        private IEmailsRepository _repository;
+        private GetAllEmailsQueryHandler _handler;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _repository = Freeze<IEmailsRepository>();
+            _handler = Create<GetAllEmailsQueryHandler>();
+        }
+
+        [Theory, AutoData]
+        public async Task ShouldFetchEmailFromRepoAndMapIt(GetAllEmailsQuery query)
+        {
+            // arrange
+            var expectedEmails = Create<List<EmailEntity>>();
+            _repository.GetAllEmails()
+                .Returns(expectedEmails.ToAsyncEnumerable());
+
+            // act
+            var actualEmails = await _handler.Handle(query, CancellationToken.None);
+
+            // assert
+            actualEmails.Should().BeEquivalentTo(expectedEmails);
+        }
+    }
+}
---
+++ MailService.Api.Tests/Queries/GetEmailQueryHandlerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture.NUnit3;
+using FluentAssertions;
+using MailService.Api.Model;
+using MailService.Api.Queries;
+using MailService.Api.Queries.Handlers;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace MailService.Api.Tests.Queries
+{
+    public class GetEmailQueryHandlerTests : TestBase
+    {
+        private IEmailsRepository _repository;
+        private GetEmailQueryHandler _handler;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _repository = Freeze<IEmailsRepository>();
+            _handler = Create<GetEmailQueryHandler>();
+        }
+
+        [Theory, AutoData]
+        public async Task ShouldFetchEmailFromRepoAndMapIt(GetEmailQuery query)
+        {
+            // arrange
+            var expectedEmail = Create<EmailEntity>();
+            _repository.GetEmail(query.Id)
+                .Returns(expectedEmail);
+
+            // act
+            var actualEmail = await _handler.Handle(query, CancellationToken.None);
+
+            // assert
+            actualEmail.Should().BeEquivalentTo(expectedEmail);
+        }
+    }
+}
