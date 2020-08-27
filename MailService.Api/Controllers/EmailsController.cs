540b88ef0def025e93a413cd6caae9dc3901ad3d
Dominik Baran <dominik.baran7@gmail.com>
2020-08-27 15:10:53 +0200

    feat: create asp.net core project from template

---
+++ MailService.Api/Controllers/EmailsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MailService.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmailsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
