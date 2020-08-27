eb6392fb8342d3f7ad8c2f1574d60f546ba1f492
Dominik Baran <dominik.baran7@gmail.com>
2020-08-28 01:41:13 +0200

    feat: added query handlers for fetching emails

---
+++ MailService.Api/Queries/Handlers/GetAllEmailsQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MailService.Api.Dto;
+using MailService.Api.Model;
+using MediatR;
+
+namespace MailService.Api.Queries.Handlers
+{
+    public class GetAllEmailsQueryHandler : IRequestHandler<GetAllEmailsQuery, IEnumerable<EmailDto>>
+    {
+        private readonly IEmailsRepository _repository;
+        private readonly IMapper _mapper;
+
+        public GetAllEmailsQueryHandler(IEmailsRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<EmailDto>> Handle(GetAllEmailsQuery request, CancellationToken cancellationToken)
+        {
+            var emails = await _repository.GetAllEmails().ToListAsync(cancellationToken);
+            return _mapper.Map<IEnumerable<EmailDto>>(emails);
+        }
+    }
+}
---
+++ MailService.Api/Queries/Handlers/GetEmailQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MailService.Api.Dto;
+using MailService.Api.Model;
+using MediatR;
+
+namespace MailService.Api.Queries.Handlers
+{
+    public class GetEmailQueryHandler : IRequestHandler<GetEmailQuery, EmailDto>
+    {
+        private readonly IEmailsRepository _repository;
+        private readonly IMapper _mapper;
+
+        public GetEmailQueryHandler(IEmailsRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<EmailDto> Handle(GetEmailQuery request, CancellationToken cancellationToken)
+        {
+            var email = await _repository.GetEmail(request.Id);
+            return _mapper.Map<EmailDto>(email);
+        }
+    }
+}
